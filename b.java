6a99c1c7fb5f6a39bd3318f90408752d3eae9b9a
Bhargav1284 <chintubargav@gmail.com>
2019-08-09 19:02:32 +0530

    final

---
+++ b.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+import java.lang.*;
+
+public class b
+{
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner(System.in);
+        int u = 0;
+        int x = 0;
+        String k = s.nextLine();
+        String p[] = k.split(" ");
+        int i = Integer.parseInt(p[0]);
+        int j = Integer.parseInt(p[1]);
+        u = i;
+        while(i > 1)
+        {
+            u = u + (i - ((int)(i*j*0.01)));
+            i = i - (int)(i*j*0.01);
+        }
+        System.out.println(u);
+    }
+}
